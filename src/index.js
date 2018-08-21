504069e8628d770f2189f8b728af22b9ebc3b144
Mats Jørgensen <matsjoer@gmail.com>
2018-08-21 21:46:30 +0200

    Add Dropdown component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Component from './lib/index';
+import { DropdownComponent } from './lib/index';
+import './index.scss';
 
-ReactDOM.render(<Component />, document.getElementById("index"));
+class App extends Component {
+
+  state = {
+    isDropdownOpen: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <button onClick={() => this.setState({ isDropdownOpen: !this.state.isDropdownOpen })}>
+          Dropdown
+        </button>
+
+        <div className="app__dropdown">
+          <DropdownComponent dropdownOpen={this.state.isDropdownOpen} onOutsideClick={() => this.setState({ isDropdownOpen: false })}>
+            <p>Hei</p>
+          </DropdownComponent>
+        </div>
+      </div>
+    );
+  }
+};
+
+
+ReactDOM.render(<App />, document.getElementById("index"));
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,84 @@
-import React, { Component } from "react";
 import './index.scss';
+import React, { Component } from 'react';
 
-class MyComponent extends Component {
+class DropdownComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.dropdownRef = React.createRef();
+  }
+
+  componentDidMount() {
+    if (this.props.dropdownOpen) {
+      this.registerWindowClickListener();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.dropdownOpen) {
+      this.registerWindowClickListener();
+    } else {
+      this.unregisterWindowClickListener();
+    }
+  }
+
+  componentWillUnmount() {
+    this.unregisterWindowClickListener();
+  }
+
+  registerWindowClickListener() {
+    if (!this.listenerRegistred) {
+      this.listenerRegistred = true;
+      window.addEventListener('mousedown', this.onWindowClick);
+    }
+  }
+
+  unregisterWindowClickListener() {
+    this.listenerRegistred = false;
+    window.removeEventListener('mousedown', this.onWindowClick);
+  }
+
+  shouldCloseDropdown(e) {
+    if (this.dropdownRef.current && this.props.dropdownOpen) {
+
+      const rect = this.dropdownRef.current.getBoundingClientRect();
+      const x = e.clientX;
+      const y = e.clientY;
+
+      if (x === 0 && y === 0) {
+        // Clicking option tag within rect causes x==y=0. Might be a bug.
+        return false;
+      }
+
+      const xWithinRect = x < rect.x + rect.width && rect.x < x;
+      const yWithinRect = y < rect.y + rect.height && rect.y < y;
+      const clickWasWithinRect = xWithinRect && yWithinRect;
+
+      console.log(`(${x}, ${y}) was within rect: ${clickWasWithinRect}`);
+      return !clickWasWithinRect;
+    }
+
+    return false;
+  }
+
+  onWindowClick = (e) => {
+    if (this.shouldCloseDropdown(e)) {
+      this.props.onOutsideClick();
+    }
+  }
 
   render() {
-    return <div>MyComponent</div>
-  }
-};
+    let dropdownHiddenAttribute = this.props.dropdownOpen ? false : true;
+    const contentModifierClass = this.props.hasOwnProperty('noPadding') ? 'dropdown__content--no-padding' : 'dropdown__content--padding';
 
-export default MyComponent;
+    return (
+      <div className="dropdown" hidden={dropdownHiddenAttribute} ref={this.dropdownRef}>
+        <div className={`dropdown__content ${contentModifierClass}`}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export { DropdownComponent };
